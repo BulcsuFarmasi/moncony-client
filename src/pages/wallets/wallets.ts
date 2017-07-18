7b9ccec3472cd35db19ca00ea6f41b050c5eef83
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-07-18 21:15:42 +0200

    add wallet add button

--- src/pages/wallets/wallets.ts
+++ src/pages/wallets/wallets.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AlertController } from 'ionic-angular'
+
 import { Wallet } from "../../models/wallet";
 import { WalletService } from "../../services/wallet.service"
 
@@ -12,10 +14,41 @@ export class WalletsPage implements OnInit {
 
     public wallets:Wallet[];
 
-    constructor(private walletService:WalletService){}
+    constructor(private walletService:WalletService, public alertController:AlertController){}
 
     ngOnInit (){
         this.getWallets();
+    }
+
+    addWallet() {
+        let prompt = this.alertController.create({
+            title: 'Új pénztárca',
+            message: 'Írd be az új pénztárca nevét',
+            inputs: [
+                {
+                    name: 'name',
+                    placeholder: 'Név'
+                },
+                {
+                    name:'currentAmount',
+                    placeholder: 'Kezdő összeg',
+                    type: 'number'
+                }
+            ],
+            buttons:[
+                {
+                    text: 'Mégse',
+                    role:'cancel'
+                },
+                {
+                   text: 'Hozzáadás',
+                   handler:wallet => {
+                       this.walletService.addWallet(wallet);
+                   }
+                }
+            ]
+        });
+        prompt.present();
     }
 
     getWallets() {
