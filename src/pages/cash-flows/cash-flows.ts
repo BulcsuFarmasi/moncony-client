0852d28954415a6bd8970608ecfd8c06a262746f
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-07-19 16:40:03 +0200

    cash flows pag basic setup

--- src/pages/cash-flows/cash-flows.ts
+++ src/pages/cash-flows/cash-flows.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core'
+import {Component, OnInit} from '@angular/core';
+
+import { NavController, NavParams } from 'ionic-angular';
+
+import { Wallet } from '../../models/wallet/wallet';
+import { CashFlow } from '../../models/cash-flow/cash-flow';
+
+import { CashFlowService } from '../../services/cash-flow/cash-flow.service';
+import { WalletService } from '../../services/wallet/wallet.service';
 
 @Component({
     selector: 'page-cash-flows',
     templateUrl: 'cash-flows.html'
 })
 
-export class CashFlowsPage {
+export class CashFlowsPage implements OnInit {
+    public cashFlows:CashFlow[];
+    public wallet:Wallet;
 
+    constructor(private cashFlowService:CashFlowService, private walletService:WalletService,
+                private navController:NavController, private navParams:NavParams){}
+
+    ngOnInit () {
+        this.getWallet();
+        this.getCashFlows();
+    }
+
+    getCashFlows () {
+        this.cashFlowService.getCashFlows(this.wallet.id)
+            .subscribe((cashFlows:CashFlow[]) => {this.cashFlows = cashFlows; console.log(this.cashFlows)});
+    }
+
+    getWallet () {
+        this.wallet=this.navParams.get('wallet');
+        console.log(this.wallet);
+    }
 }
