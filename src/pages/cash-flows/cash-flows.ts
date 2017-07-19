8574bf4291e49e1056dd8590bc78a0967e9a6345
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-07-19 19:34:06 +0200

    add add button logic

--- src/pages/cash-flows/cash-flows.ts
+++ src/pages/cash-flows/cash-flows.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 
-import { NavController, NavParams } from 'ionic-angular';
+import { AlertController, NavController, NavParams } from 'ionic-angular';
 
 import { Wallet } from '../../models/wallet/wallet';
 import { CashFlow } from '../../models/cash-flow/cash-flow';
@@ -18,7 +18,8 @@ export class CashFlowsPage implements OnInit {
     public wallet:Wallet;
 
     constructor(private cashFlowService:CashFlowService, private walletService:WalletService,
-                private navController:NavController, private navParams:NavParams){}
+                private navController:NavController, private navParams:NavParams,
+                private alertController:AlertController){}
 
     ngOnInit () {
         this.getWallet();
@@ -33,5 +34,55 @@ export class CashFlowsPage implements OnInit {
     getWallet () {
         this.wallet=this.navParams.get('wallet');
         console.log(this.wallet);
+    }
+
+    addCashFlow () {
+        let prompt = this.alertController.create();
+        prompt.setTitle('Új bevétel/kiadás');
+        prompt.setMessage('Írd be az új bevétel/kiadás adatait');
+        prompt.addInput({
+            name: 'text',
+            placeholder: 'Szöveg'
+        });
+        prompt.addInput({
+            type: 'radio',
+            name: 'income',
+            label: 'Bevétel',
+            value: '1'
+        });
+        prompt.addInput({
+            type: 'radio',
+            name: 'income',
+            label: 'Kiadás',
+            value: '-1'
+        });
+        prompt.addInput({
+            type: 'number',
+            name: 'amount',
+            placeholder: 'Összeg'
+        })
+        prompt.addButton({
+            role: 'cancel',
+            text: 'Mégse'
+        })
+        prompt.addButton({
+            text: 'Hozzáadás',
+            handler: cashFlow => {
+                cashFlow.amount *= parseInt(cashFlow.income);
+                delete cashFlow.income;
+                cashFlow.walletId = this.wallet.id
+                cashFlow.date = new Date()
+                this.cashFlowService.addCashFlow(cashFlow)
+                    .subscribe((cashFlow:CashFlow) => {
+                        this.cashFlows.push(cashFlow);
+                        let wallet = Object.assign({}, this.wallet);
+                        wallet.amount = cashFlow.amount;
+                        this.walletService.modifyWallet(wallet)
+                            .subscribe((wallet:Wallet) => this.wallet = wallet );
+                    })
+            }
+        })
+
+        prompt.present();
     }
 }
