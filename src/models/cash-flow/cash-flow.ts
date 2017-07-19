f4e84a2cf1fb3ecc65ecaf628fdc7f944391c3f0
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-07-19 11:53:42 +0200

    add cash flow service and model

---
+++ src/models/cash-flow/cash-flow.ts
@@ -0,0 +1,7 @@
+export class CashFlow {
+    public id:string;
+    public walletId:string;
+    public amount:number;
+    public text:number;
+    public date:Date
+}
---
+++ src/services/cash-flow/cash-flow.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core'
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { CashFlow } from '../../models/cash-flow/cash-flow'
+
+@Injectable()
+
+export class CashFlowService {
+    private apiUrl:string = 'http://localhost:8080/api/cash-flows/';
+    constructor(private http:Http){}
+
+    addCashFlow(cashFlow:CashFlow):Observable<CashFlow> {
+        return this.http.post(`${this.apiUrl}/${cashFlow.walletId}`, cashFlow)
+            .map((response:Response) => response.json());
+    }
+
+    getCashFlows(walletId:number): Observable<CashFlow[]>{
+        return this.http.get(`${this.apiUrl}/${walletId}`)
+            .map((response:Response) => response.json());
+    }
+
+    modifyCashFlow(cashFlow:CashFlow){
+        return this.http.put(`${this.apiUrl}/${cashFlow.id}`, cashFlow)
+            .map((response:Response) => response.json());
+    }
+
+    deleteCashFlow(cashFlowId:number){
+        return this.http.delete(`${this.apiUrl}/${cashFlowId}`)
+    }
+}
